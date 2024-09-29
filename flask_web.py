bd82c628fb60ea938297d5d25b90d335928196c8
Martin Pelíšek <161080106+MaestroMartin@users.noreply.github.com>
2024-09-29 10:51:49 +0200

    excercise

--- flask_web.py
+++ flask_web.py
@@ -1,4 +1,4 @@
-from flask import Flask, request, make_response, render_template
+from flask import Flask, request, make_response, render_template, redirect, url_for
 
 
 app = Flask(__name__, template_folder= "templates")
@@ -7,15 +7,26 @@ def index():
     mylist = [10, 20, 30, 40, 50]
     return render_template("index.html", mylist = mylist)
 
-@app.route("/other")
+@app.route("/Other")
 def Other():
     some_text = "hello World"
     return render_template("other.html", some_text = some_text)
 
-@app.template_filter("revers_string")
+@app.route('/redirect_endpoint')
+def redirect_endpoint():
+    return redirect(url_for('Other'))
+
+@app.template_filter("revers_string")   
 def revers_string(s):
     return s[::-1]
 
+@app.template_filter("repeat")
+def repeat (s, times= 2):
+    return s * times
+
+@app.template_filter("alternate_case")
+def alternate_case(s):
+    return ''.join([c.upper()if i % 2 ==0 else c.lower()for i, c in enumerate(s)])
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port= 5555, debug= True)
